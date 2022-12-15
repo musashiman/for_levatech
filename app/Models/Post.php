1544346c50af2745078af3d335173c759b92eee8
musashiman <musashiman4628@gmail.com>
2022-12-16 00:57:55 +0900

    Add getLimit

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    
+    public function getOrderBy(int $count=10)
+    {
+        return $this->orderBy("updated_at","DESC")->limit($count)->get();
+    }
 }
