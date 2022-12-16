ea3f93d1d892d6a0c3c618c433ff12a9fb158c8e
musashiman <musashiman4628@gmail.com>
2022-12-16 16:03:01 +0900

    Add show.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,27 +2,32 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        
-        <title>Blog</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
         <!-- Styles -->
-        
+    
     </head>
     <body class="antialiased">
-       <h1>Blog App</h1>
-       <div class="posts">
-           @foreach($posts as $post)
-           <div class="post">
-               <h2 class="title">{{$post->title}}</h2>
-               <p class="body"> {{$post->body}}</p>
-            @endforeach
-           </div>
-       </div>
-       <div class="paginate">
-           {{$posts->links()}}
-       </div>
+      <h1 class="title">title</h1>
+      <div class="posts">
+          <div class="post">
+              <h2>Title</h2>
+              <p>This is a sample body.</p>
+              @foreach($posts as $post)
+                <h2 class="title">
+                  <a href="/posts/{{$post->id}}">{{$post->title}}</a>
+                </h2>
+                <P class="body">{{$post->body}}</P>
+              @endforeach
+          </div>
+      </div>
+      <div class="paginate">
+          {{$posts->links()}}
+      </div>
     </body>
 </html>
